fe44cb0e684645ead807b1dfdfc6bfdcf8fd8b7e
sam <amazing_sam@outlook.com>
2019-01-12 16:13:33 +0800

    消除全局变量，使用自执行函数

--- resume/js/auto-slideup.js
+++ resume/js/auto-slideup.js
@@ -1,32 +1,34 @@
-let specialTags = document.querySelectorAll('[data-x]');
-for (let i = 0; i < specialTags.length; i++) {
-    specialTags[i].classList.add('offset');
-}
-findClosestAndRemoveOffset();
-window.addEventListener('scroll', function (x) {
-    findClosestAndRemoveOffset();
-})
-
-function findClosestAndRemoveOffset() {
+! function () {
     let specialTags = document.querySelectorAll('[data-x]');
-    let minIndex = 0;
-    //为1的原因，specialTags[0]需要和specialTags[1]比较
-    for (let i = 1; i < specialTags.length; i++) {
-        if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop -
-                window.scrollY)) {
-            minIndex = i;
+    for (let i = 0; i < specialTags.length; i++) {
+        specialTags[i].classList.add('offset');
+    }
+    findClosestAndRemoveOffset();
+    window.addEventListener('scroll', function (x) {
+        findClosestAndRemoveOffset();
+    })
+
+    function findClosestAndRemoveOffset() {
+        let specialTags = document.querySelectorAll('[data-x]');
+        let minIndex = 0;
+        //为1的原因，specialTags[0]需要和specialTags[1]比较
+        for (let i = 1; i < specialTags.length; i++) {
+            if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop -
+                    window.scrollY)) {
+                minIndex = i;
+            }
         }
+        specialTags[minIndex].classList.remove('offset');
+        let id = specialTags[minIndex].id;
+        let a = document.querySelector('a[href="#' + id + '"]');
+        //let li=a.parentNode;
+        //let brothersAndMe=a.parentNode.children;
+        let li = a.parentNode;
+        //console.log(li);
+        let brothersAndMe = li.parentNode.children;
+        for (let q = 0; q < brothersAndMe.length; q++) {
+            brothersAndMe[q].classList.remove('highlight');
+        }
+        li.classList.add('highlight');
     }
-    specialTags[minIndex].classList.remove('offset');
-    let id = specialTags[minIndex].id;
-    let a = document.querySelector('a[href="#' + id + '"]');
-    //let li=a.parentNode;
-    //let brothersAndMe=a.parentNode.children;
-    let li = a.parentNode;
-    //console.log(li);
-    let brothersAndMe = li.parentNode.children;
-    for (let q = 0; q < brothersAndMe.length; q++) {
-        brothersAndMe[q].classList.remove('highlight');
-    }
-    li.classList.add('highlight');
-}
+}.call()
--- resume/js/sticky-topbar.js
+++ resume/js/sticky-topbar.js
@@ -1,7 +1,9 @@
-window.addEventListener('scroll', function (x) {
-    if (window.scrollY > 0) {
-        header.classList.add('sticky');
-    } else {
-        header.classList.remove('sticky')
-    }
-})
+!function(){
+    window.addEventListener('scroll', function (x) {
+        if (window.scrollY > 0) {
+            header.classList.add('sticky');
+        } else {
+            header.classList.remove('sticky')
+        }
+    })
+}.call();
