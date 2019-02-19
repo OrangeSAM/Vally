eea987fd201b4d9a427aa311fcc47ad428f79723
sam <amazing_sam@outlook.com>
2019-02-19 17:28:05 +0800

    achieved route redirect follow the tutorial

---
+++ Vue/vueclitest/myvueproject/src/components/HelloShenzhen.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>this is hello shenzhen</h1>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloShenzhen",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+</style>
---
+++ Vue/vueclitest/myvueproject/src/components/list.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li>
+      <router-link to="/helloshenzhen">hello shenzhen</router-link>
+    </li>
+    <li>
+      <router-link to="/helloworld">hello world</router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "list"
+};
+</script>
+
+<style scoped>
+</style>
